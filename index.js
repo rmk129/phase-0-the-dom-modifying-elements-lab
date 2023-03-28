7015dc9f06bbb598dbe435fa7dc64fbc21c7274f
Romil <romil.m.kothari@gmail.com>
2023-03-28 09:59:42 -0400

    tests completed

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-// Write your code here!
+// // Write your code here!
+// const main = document.getElementById("main");
+// main.remove();
+// const newHeader = document.createElement("h1");
+// newHeader.id = 'victory';
+// newHeader.textContent = 'Romil Kothari is the champion';
+// document.body.append(newHeader);
+
+
 const main = document.getElementById("main");
 main.remove();
 const newHeader = document.createElement("h1");
-newHeader.id = 'victory';
-newHeader.textContent = 'Romil Kothari is the champion';
-document.body.append(newHeader);
-
-
+newHeader.id = "victory";
+newHeader.textContent = "Romil is the champion";
